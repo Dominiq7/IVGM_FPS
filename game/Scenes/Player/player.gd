f7c3f2ceb21dddc0c943f2b8ae376baa529fee29
Junqing <yimingtong305@gmail.com>
2023-10-04 19:39:40 +0200

    adjust magic wand position and add sprint function

--- game/Scenes/Player/player.gd
+++ game/Scenes/Player/player.gd
@@ -6,7 +6,9 @@ extends CharacterBody3D
 var mouseSensibility = 1200
 var mouse_relative_x = 0
 var mouse_relative_y = 0
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -24,6 +26,13 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	# Handle Sprint.
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	
 	# Handle Shooting
 	if Input.is_action_just_pressed("Shoot"):
 		shoot()
@@ -31,11 +40,11 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	# Handle Ability
 	if Input.is_action_just_pressed("Ability"):
